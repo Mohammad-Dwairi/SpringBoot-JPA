986e1b0e6b4a01c354a089b0248a9cb6efaacf24
Mohammad_Dwairi <mdwairy@gmail.com>
2021-01-12 15:15:26 +0200

    Create demo Unit test on Category class

---
+++ src/test/java/mdwairy/spring/meals/models/CategoryTest.java
@@ -0,0 +1,25 @@
+package mdwairy.spring.meals.models;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryTest {
+
+    Category category;
+
+    @BeforeEach // @BeforeEach runs before the execution of each test
+    public void setUp() {
+        category = new Category();
+    }
+    @Test // Test1
+    void getDescription() {
+    }
+
+    @Test // Test2
+    void getId() {
+        category.setId(4L);
+        assertEquals(4L, category.getId());
+    }
+}
